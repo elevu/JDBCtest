7191d4dd2d2d9a020b65d83fa135372ee6cd35b7
EleonoraMaccacaro <eleonora.maccacaro@gmail.com>
2017-12-06 10:38:42 +0100

    Added two classes

---
+++ src/jdbctest/Artist.java
@@ -0,0 +1,19 @@
+
+package jdbctest;
+
+
+public class Artist {
+   private int id;
+   private String name;
+   private String lastName;
+   private int age;
+   
+   
+    public Artist(int id, String name, String lastName, int age) {
+       this.id = id;
+       this.name = name;
+       this.lastName = lastName;
+       this.age = age;
+    }
+    
+}
